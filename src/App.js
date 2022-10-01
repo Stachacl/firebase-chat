e0aefe84643ba0fd7541e497503aeda2d75f09eb
Stachacl <stacha.l@icloud.com>
2022-10-01 20:59:41 +1000

    styling app with tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 
+const style = {
+  appContainer: `max-w-[728px] mx-auto text-center`
+}
+
 
 function App() {
   return (
-    <div>
-     <h1 class="text-3xl font-bold "> hi </h1>
+    <div className="max-w-[728px] mx-auto text-center">
+      <section className="flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative">
+      <h1> hi </h1>
+      </section>
     </div>
   );
 }
