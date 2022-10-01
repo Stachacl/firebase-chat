0ad9f61817d9e19a6b4f29a8bd5b7a840b031425
Stachacl <stacha.l@icloud.com>
2022-10-01 21:02:40 +1000

    separated styles into separate block

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 
 const style = {
-  appContainer: `max-w-[728px] mx-auto text-center`
+  appContainer: `max-w-[728px] mx-auto text-center`,
+  sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`
+
 }
 
 
 function App() {
   return (
-    <div className="max-w-[728px] mx-auto text-center">
-      <section className="flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative">
+    <div className={style.appContainer}>
+      <section className={style.sectionContainer}>
       <h1> hi </h1>
       </section>
     </div>
