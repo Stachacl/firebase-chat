e5149c074bf621257beac6be31889e1435fd7f8c
Stachacl <stacha.l@icloud.com>
2022-10-01 21:06:42 +1000

    Navbar created and imported to the App.ja

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
 
 const style = {
   appContainer: `max-w-[728px] mx-auto text-center`,
-  sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`
-
-}
-
+  sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
+};
 
 function App() {
   return (
     <div className={style.appContainer}>
       <section className={style.sectionContainer}>
-      <h1> hi </h1>
+        < Navbar />
+        {/* Chat component */}
       </section>
     </div>
   );
