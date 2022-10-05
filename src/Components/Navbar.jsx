8445c8d25203c48429b0d48286d715c562252290
Stachacl <stacha.l@icloud.com>
2022-10-05 21:56:31 +1100

    log out component created

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Signin from "./Signin";
+import LogOut from "./LogOut";
 
 const style = {
 nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
@@ -15,6 +16,7 @@ const Navbar = () => {
     <div className={style.nav}>
       <h1 className={style.heading}>Navbar</h1>
       <Signin />
+      <LogOut />
     </div>
   );
 };
