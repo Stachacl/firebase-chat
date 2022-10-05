28d09ae58c5d6ac6ce6a6c09dc66181de70c5213
Stachacl <stacha.l@icloud.com>
2022-10-05 18:51:11 +1100

    google button added

--- src/Components/Signin.jsx
+++ src/Components/Signin.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import GoogleButton from 'react-google-button'
+
+const style = {
+    wrapper: `bg-gray-800`
+    }
 
 const Signin = () => {
   return (
-    <div>Signin</div>
+    <div className={style.wrapper}>
+      <GoogleButton />
+      </div>
   )
 }
 
