4ff25a32a686f0f690414e758d10129afe4d4fec
Stachacl <stacha.l@icloud.com>
2022-10-05 22:12:52 +1100

    log out functionality done

--- src/Components/LogOut.jsx
+++ src/Components/LogOut.jsx
@@ -5,10 +5,15 @@ const style = {
     button: "inline-flex  shadow items-center justify-center border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700"
 }
 
-
 const LogOut = () => {
+    const signOut = () => {
+        signOut(auth)
+    }
+
   return (
-    <div className={style.button}>LogOut</div>
+    <div onClick={ () => auth.signOut()} className={style.button}>
+        LogOut
+    </div>
   )
 }
 
