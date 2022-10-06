1e68beefd6cf21d520bde48c8434b3cc8bdd9d9c
Stachacl <stacha.l@icloud.com>
2022-10-06 14:09:00 +1100

    input form created

--- src/Components/SendMessage.jsx
+++ src/Components/SendMessage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
+const  style = {
+    form: `h-14 w-full max-w-[728px] flex text-xl absolute bottom-0`,
+    input: `w-full text-xl p-3 bg-gray-900 text-white outline-none border-none`,
+    button: `w-[20%] bg-blue-500`
+}
+
 const SendMessage = () => {
   return (
-    <div>SendMessage</div>
+    <form className={style.form}>
+        <input className={style.input} 
+        type="text" placeholder='Type something' />
+        <button className={style.button} type="submit">Send</button>
+    </form>
+   
   )
 }
 
