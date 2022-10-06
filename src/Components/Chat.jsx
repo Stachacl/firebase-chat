3348db98ed0d312954e4da49000b83b9595d843f
Stachacl <stacha.l@icloud.com>
2022-10-06 13:44:12 +1100

    imported functions from firebase

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,6 +1,8 @@
-import { onSnapshot } from "firebase/firestore";
+import { onSnapshot, query, collection, orderBy } from "firebase/firestore";
+import { db } from "../firebase";
 import React, { useState, useEffect, useRef } from "react";
 import Message from "./Message";
+
 
 const style = {
   main: `flex flex-col p-[10px] relative`,
@@ -12,12 +14,15 @@ const Chat = () => {
 
   useEffect(() => {
     const q = query(collection(db, "firebase-chat"), orderBy("timestamp"));
+
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       let messages = [];
       querySnapshot.forEach((doc) => {
         messages.push({ ...doc.data(), id: doc.id });
-      }); 
+      });
+      setMessages(messages);
     });
+    return () => unsubscribe();
   }, []);
 
   return (
