f9e341990c8caa446ad174316f7f740518ce4688
Stachacl <stacha.l@icloud.com>
2022-10-06 13:36:43 +1100

    use effect to see updated in firebase data base in real time and update it:

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useRef} from "react";
+import { onSnapshot } from "firebase/firestore";
+import React, { useState, useEffect, useRef } from "react";
 import Message from "./Message";
 
 const style = {
@@ -6,20 +7,26 @@ const style = {
 };
 
 const Chat = () => {
-  const [messages, setMessages] =useState([]);
-  const scroll = useRef()
+  const [messages, setMessages] = useState([]);
+  const scroll = useRef();
 
-useEffect (() => {
-    const q =query(collection(db, 'firebase-chat'), orderBy('timestamp'))
-}, [])
+  useEffect(() => {
+    const q = query(collection(db, "firebase-chat"), orderBy("timestamp"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let messages = [];
+      querySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      }); 
+    });
+  }, []);
 
   return (
     <>
       <main className={style.main}>
-       <Message />
-        </main>
-         {/* Send message component -form */}
-         <span ref={scroll}></span>
+        <Message />
+      </main>
+      {/* Send message component -form */}
+      <span ref={scroll}></span>
     </>
   );
 };
