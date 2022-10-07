09e5caab0bc22c382f30966dbdab04198f8f04cd
Stachacl <stacha.l@icloud.com>
2022-10-07 15:09:12 +1100

    styles

--- src/Components/SendMessage.jsx
+++ src/Components/SendMessage.jsx
@@ -3,9 +3,9 @@ import React, {useState} from 'react'
 import { auth, db } from '../firebase'
 
 const  style = {
-    form: `h-14 w-full max-w-[728px] flex text-xl absolute bottom-0`,
+    form: `h-14 w-full max-w-[728px] flex text-xl absolute bottom-0 `,
     input: `w-full text-xl p-3 bg-gray-900 bg-opacity-80 text-white outline-none border-none`,
-    button: `w-[20%] bg-blue-500`
+    button: `w-[20%] bg-indigo-400`
 }
 
 const SendMessage = ({scroll}) => {
