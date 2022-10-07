36301f77756d73d68219d658cee3c5c1ee984b44
Stachacl <stacha.l@icloud.com>
2022-10-07 13:54:10 +1100

    prevent sending empty message

--- src/Components/SendMessage.jsx
+++ src/Components/SendMessage.jsx
@@ -4,7 +4,7 @@ import { auth, db } from '../firebase'
 
 const  style = {
     form: `h-14 w-full max-w-[728px] flex text-xl absolute bottom-0`,
-    input: `w-full text-xl p-3 bg-gray-900 text-white outline-none border-none`,
+    input: `w-full text-xl p-3 bg-gray-900 bg-opacity-80 text-white outline-none border-none`,
     button: `w-[20%] bg-blue-500`
 }
 
@@ -13,6 +13,11 @@ const [userMessage, setUserMessage] = useState ('');
 
 const sendMessage = async (e) => {
     e.preventDefault()
+
+    if (userMessage === '') {
+      alert ('Type something...')
+    }
+
     const {uid, displayName} = auth.currentUser
     await addDoc(collection(db, 'firebase-chat'), {
         text: userMessage,
