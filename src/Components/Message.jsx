24d5b63bf5b0fa42cbb349d3fff1261489c63d5b
Stachacl <stacha.l@icloud.com>
2022-10-07 14:44:58 +1100

    styles of messages fixed

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { auth } from '../firebase'
 
 const style = {
-    message:`flex items-center shadow-xl m-4 py-2 px-3 rounded-tr-full`,
+    message:`flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
     name: `absolute mt-[-4rem] text-gray-600 text-xs`,
-    sent:`bg-blue text-white flex-row-reverse text-end float-right rounded-br-full`,
-    received:`bg-yellow text-black float-left rounded-br-full`,
+    sent:`bg-gray-500 text-white flex-row-reverse text-end float-right rounded-bl-full`,
+    received:`bg-blue-500 text-black float-left rounded-br-full`,
 }
 
 const Message = ({message}) => {
